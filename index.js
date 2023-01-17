8a7301ce6d74a45d7564345651c43969e9e82829
Bojan <bdragojevic@gmail.com>
2023-01-17 12:11:35 +0100

    initial basic GPT chat setup

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Openai = require('openai');
+
+const { Configuration, OpenAIApi } = Openai;
+
+const app = express();
+
+const configuration = new Configuration({
+  organization: 'org-YPbLcjMBXnzdBm1gc2eOaFmk',
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/api', async (req, res) => {
+  const { message } = req.body;
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: message,
+    max_tokens: 7,
+    temperature: 0,
+  });
+
+  if (response.data?.choices) {
+    res.json({
+      message: response.data?.choices[0],
+    });
+  }
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('http://localhost:3001/api', {
+      method: 'POST',
+      body: JSON.stringify({ message }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    setResponse(data.message.text);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea value={message} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+      <div>{response && <p>{response}</p>}</div>
     </div>
   );
-}
+};
 
 export default App;
