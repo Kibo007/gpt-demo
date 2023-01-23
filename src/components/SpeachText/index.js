4395c1962bfea76ff95b1469a069aec2e7a1cc06
Bojan <bdragojevic@gmail.com>
2023-01-23 19:46:28 +0100

    add message for mobile devices

--- src/components/SpeachText/index.js
+++ src/components/SpeachText/index.js
@@ -3,7 +3,21 @@ import PropTypes from 'prop-types';
 
 export const SpeachText = ({ speachText }) => {
   return (
-    <div className={styles.speachTextContainer}>{speachText && speachText}</div>
+    <>
+      <div className={styles.speachTextContainer}>
+        {speachText && speachText}
+      </div>
+      <div className={styles.mobileMessage}>
+        <span>
+          SpeechRecognition browser api is not working properly in mobile
+          devices. For best expirience please use desktop device.{' '}
+        </span>
+
+        <a href="https://udn.realityripple.com/docs/Web/API/Web_Speech_API">
+          Browser compatibility
+        </a>
+      </div>
+    </>
   );
 };
 
